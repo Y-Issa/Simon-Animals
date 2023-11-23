6e3aaa3d286f51ffca6812145a1fe7e3f545337d
Youssef Issa <151861282+Y-Issa@users.noreply.github.com>
2023-11-23 17:56:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+var animals = ["cat", "dog", "horse", "bird"];
+var gameSequence = [];
+var userSequence = [];
+
+var level = 0;
+var started = false;
+
+$(document).keydown(function () {
+  if (!started) {
+    $("h1").text("Level: " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".button").click(function () {
+  var chosenAnimal = this.id;
+  playSound(chosenAnimal);
+  userSequence.push(chosenAnimal);
+  animatePress(chosenAnimal);
+  answerCheck(userSequence.length - 1);
+});
+
+function nextSequence() {
+  var rndNum = Math.floor(Math.random() * 4);
+  var rndAnimal = animals[rndNum];
+  gameSequence.push(rndAnimal);
+  $("#" + rndAnimal)
+    .fadeOut(180)
+    .fadeIn(180);
+  playSound(rndAnimal);
+  level++;
+  $("h1").text("Level: " + level);
+}
+
+function playSound(animal) {
+  var audio = new Audio("sounds/" + animal + ".mp3");
+  audio.play();
+}
+
+function animatePress(thisAnimal) {
+  $("#" + thisAnimal).addClass("pressed");
+  setTimeout(function () {
+    $("#" + thisAnimal).removeClass("pressed");
+  }, 160);
+}
+
+function answerCheck(level) {
+  if (userSequence[level] === gameSequence[level]) {
+    if (userSequence.length === gameSequence.length) {
+      setTimeout(function () {
+        nextSequence();
+        userSequence = [];
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+      restart();
+    }, 200);
+    $("h1").text("Game Over, press any key to restart");
+  }
+}
+
+function restart() {
+  gameSequence = [];
+  userSequence = [];
+  level = 0;
+  started = false;
+}
